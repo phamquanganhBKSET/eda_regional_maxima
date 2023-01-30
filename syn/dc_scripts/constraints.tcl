de970cb6ebbbc3f84c45a36ce8abe80164d69392
Phạm Quang Anh <78630400+phamquanganhBKSET@users.noreply.github.com>
2023-01-30 01:30:46 +0000

    Update constraints.tcl

--- syn/dc_scripts/constraints.tcl
+++ syn/dc_scripts/constraints.tcl
@@ -4,10 +4,6 @@ create_clock -period $CLOCK_PERIOD_DTI [get_ports clk]
 
 set_clock_uncertainty $MARGIN_DTI [get_ports clk]
 
-# set_clock_transition -max $MAX_TRANSITION_TIME [get_clock clk]
-
-# set_clock_latency -source -max 1.2 [get_clock clk]
-
 set_input_delay -max 0.4 -clock clk [get_ports reset_n  ]
 set_input_delay -max 0.4 -clock clk [get_ports pixel_in*]
 set_input_delay -max 0.4 -clock clk [get_ports start    ]
